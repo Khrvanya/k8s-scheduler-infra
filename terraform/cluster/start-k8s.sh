b81f227b409564a025d7f4a077d7a1feaaa20881
Ivan <khrvanya@gmail.com>
2024-03-22 00:03:46 +0000

    pluginConfig

--- terraform/cluster/start-k8s.sh
+++ terraform/cluster/start-k8s.sh
@@ -1,3 +1,4 @@
+# connect
 gcloud container clusters get-credentials --project="saas-test-omfe" "scheduler-us-central1-a" --zone="us-central1-a"
 gcloud compute ssh --project="shared-host-dev-n47z" "bastion-host-dev" --zone="us-central1-a" --tunnel-through-iap --ssh-flag="-4 -L8888:localhost:8888 -N -q -f"
 
